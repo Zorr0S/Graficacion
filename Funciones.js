25dbea258e8c6ab9684eb267010c211f94fb5ddc
Zorr0S <86990362+Zorr0S@users.noreply.github.com>
2022-02-04 14:43:27 -0700

    Salva vidaS

--- Funciones.js
+++ Funciones.js
@@ -1,42 +1,160 @@
+"use strict";
 //TODO:Validar cuando solo se dibuja un punto
-function MetodoBasico(X0, Y0, X1, Y1) {
-    var m = Pendiente(X0, Y0, X1, Y1);
-    //console.log(`${Y0} - (${m} * ${X0})`)
-    var B = Y0 - m * X0;
-    var Dx = Math.abs(X0 - X1);
-    var Dy = Math.abs(Y0 - Y1);
-    var TablaCoordenadas = new Array();
-    TablaCoordenadas.push([0, 0]);
-    TablaCoordenadas[0][0] = X0;
-    TablaCoordenadas[0][1] = Y0;
-    function Normal(Valor) {
-        //console.log(`(${m}(${Valor}) )+ ${B} = ${((m*(Valor))+B)}`);
-        return m * Valor + B;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var rect = canvas.getBoundingClientRect();
+var inicialX, inicialY;
+var finalX, finalY;
+var Seleccion = "Directo";
+//-----------------------Botonoes--------------------------
+canvas.addEventListener("mousedown", function (e) {
+    inicialX = e.clientX - rect.left;
+    inicialY = e.clientY - rect.top;
+    console.log("Coordenada 1: ".concat(inicialX, ", ").concat(inicialY));
+});
+canvas.addEventListener("mouseup", function (e) {
+    finalX = e.clientX - rect.left;
+    finalY = e.clientY - rect.top;
+    console.log("Coordenada 2: ".concat(finalX, ", ").concat(finalY));
+    Selector(Seleccion, inicialX, inicialY, finalX, finalY);
+});
+function handleTool(X) {
+    console.log(X.value);
+    Seleccion = X.value;
+}
+//ALgoritmos
+function Selector(Metodo, X0, Y0, X1, Y1) {
+    var Xi = X0, Xf = X1;
+    var Yi = Y0, Yf = Y1;
+    switch (Metodo) {
+        case "DDA":
+            console.log("Entro DDA");
+            if (!(Y0 <= Y1)) {
+                var AuxiliarX = X0;
+                var AuxiliarY = Y0;
+                // En caso de que pase sus valores se intercambian
+                Y0 = Y1;
+                Y1 = AuxiliarY;
+                X0 = X1;
+                X1 = AuxiliarX;
+            }
+            DibujarLineaDDA(Math.round(X0), Math.round(Y0), Math.round(X1), Math.round(Y1));
+            break;
+        default:
+            console.log("Entro Directo");
+            if (X0 > X1) {
+                console.log("Se invertierosn las cords");
+                Xi = X1;
+                Xf = X0;
+                Yi = Y1;
+                Yf = Y0;
+            }
+            DibujarLineaDirecta(Math.round(Xi), Math.round(Yi), Math.round(Xf), Math.round(Yf));
+            break;
     }
-    function ANormal(Valor) {
-        //console.log(`${Valor} ${B} / ${m}`)
-        return (Valor - B) / m;
+}
+//----------------------------Metodos
+function MetodoDirecto(X0, Y0, X1, Y1) {
+    var Pendiente, B, DeltaX, DeltaY;
+    DeltaX = X1 - X0;
+    DeltaY = Y1 - Y0;
+    if (Math.abs(DeltaX) > Math.abs(DeltaY)) {
+        // Pendiente < 1
+        Pendiente = DeltaY / DeltaX;
+        B = Y0 - Pendiente * X0;
+        if (DeltaX < 0) {
+            DeltaX = -1;
+        }
+        else {
+            DeltaX = 1;
+        }
+        while (X0 != X1) {
+            X0 += DeltaX;
+            Y0 = Math.round(Pendiente * X0 + B);
+            DibujarPixel(X0, Y0);
+        }
     }
-    if (Dy > Dx) {
-        //Funcion Despejada
-        for (var index = 1; index < Dy; index++) {
-            var Aux = TablaCoordenadas[0][1] + index;
-            var ValorX = Math.round(ANormal(Aux));
-            TablaCoordenadas.push([ValorX, Aux]);
+    else if (DeltaY != 0) {
+        // Pendiente >= 1
+        Pendiente = DeltaX / DeltaY;
+        B = X0 - Pendiente * Y0;
+        if (DeltaY < 0) {
+            DeltaY = -1;
+        }
+        else {
+            DeltaY = 1;
+        }
+        while (Y0 != Y1) {
+            Y0 += DeltaY;
+            X0 = Math.round(Pendiente * Y0 + B);
+            DibujarPixel(X0, Y0);
+        }
+    }
+}
+function MetodoDDA(X0, Y0, X1, Y1) {
+    var DeltaX = Math.abs(X1 - X0);
+    var DeltaY = Math.abs(Y1 - Y0);
+    var Pendiente;
+    // Cambiar puntos aqui, no antes
+    if (DeltaY > DeltaX) {
+        Pendiente = (X1 - X0) / (Y1 - Y0);
+        // Se calculan las X
+        // Se debe saber si y0 es mayor a y1, porque si esto pasa nunca entrara al ciclo for
+        // if (!(Y0 <= Y1)) {
+        //   let AuxiliarX: number = X0;
+        //   let AuxiliarY: number = Y0;
+        //   // En caso de que pase sus valores se intercambian
+        //   Y0 = Y1;
+        //   Y1 = AuxiliarY;
+        //   X0 = X1;
+        //   X1 = AuxiliarX;
+        // }
+        var ValorX = X0;
+        for (ValorY = Y0; ValorY <= Y1; ValorY++) {
+            DibujarPixel(Math.round(ValorX), ValorY);
+            ValorX += Pendiente;
         }
     }
     else {
-        //Se inicializa el array
-        console.log(Dx);
-        for (var index = 1; index < Dx; index++) {
-            var Aux = TablaCoordenadas[0][0] + index;
-            // console.log(`${TablaCoordenadas[0][0]} + ${index} = ${Aux}`);
-            //console.log(index);
-            var ValorY = Math.round(Normal(Aux));
-            TablaCoordenadas.push([Aux, ValorY]);
+        Pendiente = (Y1 - Y0) / (X1 - X0);
+        // Se calculan las Y
+        // Se debe saber si x0 es mayor a x1, porque si esto pasa nunca entrara al ciclo for
+        if (!(X0 <= X1)) {
+            var AuxioliarY = Y0;
+            var AuxiliarX = X0;
+            // En caso de que pase sus valores se intercambian
+            X0 = X1;
+            X1 = AuxiliarX;
+            Y0 = Y1;
+            Y1 = AuxioliarY;
+        }
+        var ValorY = Y0;
+        for (ValorX = X0; ValorX <= X1; ValorX++) {
+            DibujarPixel(ValorX, Math.round(ValorY));
+            ValorY += Pendiente;
         }
     }
-    return TablaCoordenadas;
+}
+function MetodoBresenHan(X0, Y0, x1, y1) {
+    var DeltaX = Math.abs(x1 - X0);
+    var DeltaY = Math.abs(y1 - Y0);
+    var sx = X0 < x1 ? 1 : -1;
+    var sy = Y0 < y1 ? 1 : -1;
+    var err = DeltaX - DeltaY;
+    while (true) {
+        DibujarPixel(X0, Y0);
+        if (X0 === x1 && Y0 === y1)
+            break;
+        var e2 = 2 * err;
+        if (e2 > -DeltaY) {
+            err -= DeltaY;
+            X0 += sx;
+        }
+        if (e2 < DeltaX) {
+            err += DeltaX;
+            Y0 += sy;
+        }
+    }
 }
 function Pendiente(X0, Y0, X1, Y1) {
     var m = (Y1 - Y0) / (X1 - X0);
@@ -50,81 +168,21 @@ function PendienteInversa(X0, Y0, X1, Y1) {
 }
 //Funcion para dibujar un solo pixel
 function DibujarPixel(X, Y) {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
     context.fillStyle = "#197BBD";
-    context.fillRect(X + 100, 100 - Y, 3, 3);
+    context.fillRect(X, Y, 3, 3);
     context.stroke();
-}
-function MetodoDDA(X0, Y0, X1, Y1) {
-    console.log("Entro Metodo DD");
-    var m = Pendiente(X0, Y0, X1, Y1);
-    var mInv = PendienteInversa(X0, Y0, X1, Y1);
-    //console.log(`${Y0} - (${m} * ${X0})`)
-    var Dx = Math.abs(X0 - X1);
-    var Dy = Math.abs(Y0 - Y1);
-    var TablaCoordenadas = new Array();
-    TablaCoordenadas.push([0, 0]);
-    TablaCoordenadas[0][0] = X0;
-    TablaCoordenadas[0][1] = Y0;
-    function Normal(Valor) {
-        return Valor + m;
-    }
-    function ANormal(Valor) {
-        return Valor + mInv;
-    }
-    if (Dy > Dx) {
-        //Funcion Despejada
-        //Indice Aux mantien el indice anterior durante el ciclo
-        var indiceAux = 0;
-        for (var index = 1; index < Dy; index++) {
-            var Aux = TablaCoordenadas[0][1] + index;
-            var ValorX = Math.round(ANormal(TablaCoordenadas[indiceAux][1]));
-            TablaCoordenadas.push([ValorX, Aux]);
-            indiceAux++;
-        }
-    }
-    else {
-        //Se inicializa el array
-        var indiceAux = 0;
-        for (var index = 1; index < Dx; index++) {
-            var Aux = TablaCoordenadas[0][0] + index;
-            // console.log(`${TablaCoordenadas[0][0]} + ${index} = ${Aux}`);
-            //console.log(index);
-            var ValorY = Math.round(Normal(TablaCoordenadas[indiceAux][1]));
-            TablaCoordenadas.push([Aux, ValorY]);
-            indiceAux++;
-        }
-    }
-    return TablaCoordenadas;
 }
 function DibujarLineaDDA(X0, Y0, X1, Y1) {
     console.log(X0 + " + " + Y0);
     console.log(X1 + " + " + Y1);
-    var TablaCoordenadas = new Array();
-    TablaCoordenadas = MetodoDDA(X0, Y0, X1, Y1);
-    console.log(TablaCoordenadas);
     DibujarPixel(X0, Y0);
+    MetodoDDA(X0, Y0, X1, Y1);
     DibujarPixel(X1, Y1);
-    console.log(TablaCoordenadas.length);
-    for (var i = 0; i < TablaCoordenadas.length; i++) {
-        var X = TablaCoordenadas[i][0];
-        var Y = TablaCoordenadas[i][1];
-        DibujarPixel(X, Y);
-    }
 }
 function DibujarLineaDirecta(X0, Y0, X1, Y1) {
     console.log(X0 + " + " + Y0);
     console.log(X1 + " + " + Y1);
-    var TablaCoordenadas = new Array();
-    TablaCoordenadas = MetodoBasico(X0, Y0, X1, Y1);
-    console.log(TablaCoordenadas);
     DibujarPixel(X0, Y0);
+    MetodoDirecto(X0, Y0, X1, Y1);
     DibujarPixel(X1, Y1);
-    console.log(TablaCoordenadas.length);
-    for (var i = 0; i < TablaCoordenadas.length; i++) {
-        var X = TablaCoordenadas[i][0];
-        var Y = TablaCoordenadas[i][1];
-        DibujarPixel(X, Y);
-    }
 }
--- Funciones.ts
+++ Funciones.ts
@@ -1,53 +1,201 @@
 //TODO:Validar cuando solo se dibuja un punto
-function MetodoBasico(
+let canvas: HTMLCanvasElement = document.getElementById(
+  "canvas"
+) as HTMLCanvasElement;
+let context: CanvasRenderingContext2D = canvas.getContext(
+  "2d"
+) as CanvasRenderingContext2D;
+let rect: DOMRect = canvas.getBoundingClientRect();
+
+let inicialX: number, inicialY: number;
+let finalX: number, finalY: number;
+
+let Seleccion: string = "Directo";
+//-----------------------Botonoes--------------------------
+canvas.addEventListener("mousedown", function (e) {
+  inicialX = e.clientX - rect.left;
+  inicialY = e.clientY - rect.top;
+  console.log(`Coordenada 1: ${inicialX}, ${inicialY}`);
+});
+canvas.addEventListener("mouseup", function (e) {
+  finalX = e.clientX - rect.left;
+  finalY = e.clientY - rect.top;
+
+  console.log(`Coordenada 2: ${finalX}, ${finalY}`);
+  Selector(Seleccion, inicialX, inicialY, finalX, finalY);
+});
+function handleTool(X: any) {
+  console.log(X.value);
+  Seleccion = X.value;
+}
+//ALgoritmos
+function Selector(
+  Metodo: string,
   X0: number,
   Y0: number,
   X1: number,
   Y1: number
-): number[][] {
-  console.log("Entro Metodo Basico")
-  let m = Pendiente(X0, Y0, X1, Y1);
-  //console.log(`${Y0} - (${m} * ${X0})`)
-  let B = Y0 - m * X0;
-  let Dx = Math.abs(X0 - X1);
+) {
+  let Xi: number = X0,
+    Xf: number = X1;
+  let Yi: number = Y0,
+    Yf: number = Y1;
+  
 
-  let Dy = Math.abs(Y0 - Y1);
-
-  let TablaCoordenadas: number[][] = new Array();
-  TablaCoordenadas.push([0, 0]);
-  TablaCoordenadas[0][0] = X0;
-  TablaCoordenadas[0][1] = Y0;
-  function Normal(Valor: number) {
-    //console.log(`(${m}(${Valor}) )+ ${B} = ${((m*(Valor))+B)}`);
-    return m * Valor + B;
-  }
-  function ANormal(Valor: number) {
-    //console.log(`${Valor} ${B} / ${m}`)
-    return (Valor - B) / m;
-  }
-
-  if (Dy > Dx) {
-    //Funcion Despejada
+  switch (Metodo) {
+    case "DDA":
+      console.log("Entro DDA");
+      if (!(Y0 <= Y1)) {
+          let AuxiliarX: number = X0;
+          let AuxiliarY: number = Y0;
+          // En caso de que pase sus valores se intercambian
+          Y0 = Y1;
+          Y1 = AuxiliarY;
     
-    for (let index: number = 1; index < Dy; index++) {
-      let Aux = TablaCoordenadas[0][1] + index;
-      let ValorX = Math.round(ANormal(Aux));
-      TablaCoordenadas.push([ValorX, Aux]);
+          X0 = X1;
+          X1 = AuxiliarX;
+        }
+      DibujarLineaDDA(
+        Math.round(X0),
+        Math.round(Y0),
+        Math.round(X1),
+        Math.round(Y1)
+      );
+      break;
+
+    default:
+      console.log("Entro Directo");
+      if (X0 > X1) {
+        console.log("Se invertierosn las cords");
+        Xi = X1;
+        Xf = X0;
+    
+        Yi = Y1;
+        Yf = Y0;
+      }
+      DibujarLineaDirecta(
+        Math.round(Xi),
+        Math.round(Yi),
+        Math.round(Xf),
+        Math.round(Yf)
+      );
+      break;
+  }
+}
+//----------------------------Metodos
+function MetodoDirecto(X0: number, Y0: number, X1: number, Y1: number) {
+  let Pendiente, B, DeltaX, DeltaY;
+  DeltaX = X1 - X0;
+  DeltaY = Y1 - Y0;
+
+  if (Math.abs(DeltaX) > Math.abs(DeltaY)) {
+    // Pendiente < 1
+    Pendiente = DeltaY / DeltaX;
+    B = Y0 - Pendiente * X0;
+    if (DeltaX < 0) {
+      DeltaX = -1;
+    } else {
+      DeltaX = 1;
+    }
+    while (X0 != X1) {
+      X0 += DeltaX;
+      Y0 = Math.round(Pendiente * X0 + B);
+      DibujarPixel(X0, Y0);
+    }
+  } else if (DeltaY != 0) {
+    // Pendiente >= 1
+    Pendiente = DeltaX / DeltaY;
+    B = X0 - Pendiente * Y0;
+    if (DeltaY < 0) {
+      DeltaY = -1;
+    } else {
+      DeltaY = 1;
+    }
+    while (Y0 != Y1) {
+      Y0 += DeltaY;
+      X0 = Math.round(Pendiente * Y0 + B);
+      DibujarPixel(X0, Y0);
+    }
+  }
+}
+function MetodoDDA(X0: number, Y0: number, X1: number, Y1: number) {
+  let DeltaX = Math.abs(X1 - X0);
+  let DeltaY = Math.abs(Y1 - Y0);
+
+  var Pendiente;
+
+  // Cambiar puntos aqui, no antes
+
+  if (DeltaY > DeltaX) {
+    Pendiente = (X1 - X0) / (Y1 - Y0);
+
+    // Se calculan las X
+    // Se debe saber si y0 es mayor a y1, porque si esto pasa nunca entrara al ciclo for
+    // if (!(Y0 <= Y1)) {
+    //   let AuxiliarX: number = X0;
+    //   let AuxiliarY: number = Y0;
+    //   // En caso de que pase sus valores se intercambian
+    //   Y0 = Y1;
+    //   Y1 = AuxiliarY;
+
+    //   X0 = X1;
+    //   X1 = AuxiliarX;
+    // }
+
+    var ValorX = X0;
+
+    for (ValorY = Y0; ValorY <= Y1; ValorY++) {
+      DibujarPixel(Math.round(ValorX), ValorY);
+
+      ValorX += Pendiente;
     }
   } else {
-    //Se inicializa el array
-    console.log(Dx);
-    for (let index: number = 1; index < Dx; index++) {
-      let Aux: number = TablaCoordenadas[0][0] + index;
-      // console.log(`${TablaCoordenadas[0][0]} + ${index} = ${Aux}`);
-      //console.log(index);
-      let ValorY: number = Math.round(Normal(Aux));
-      TablaCoordenadas.push([Aux, ValorY]);
+    Pendiente = (Y1 - Y0) / (X1 - X0);
+
+    // Se calculan las Y
+    // Se debe saber si x0 es mayor a x1, porque si esto pasa nunca entrara al ciclo for
+    if (!(X0 <= X1)) {
+      var AuxioliarY: number = Y0;
+      var AuxiliarX: number = X0;
+      // En caso de que pase sus valores se intercambian
+      X0 = X1;
+      X1 = AuxiliarX;
+
+      Y0 = Y1;
+      Y1 = AuxioliarY;
+    }
+
+    var ValorY = Y0;
+
+    for (ValorX = X0; ValorX <= X1; ValorX++) {
+      DibujarPixel(ValorX, Math.round(ValorY));
+
+      ValorY += Pendiente;
     }
   }
-  return TablaCoordenadas;
 }
+function MetodoBresenHan(X0: number, Y0: number, x1: number, y1: number) {
+  let DeltaX = Math.abs(x1 - X0);
+  let DeltaY = Math.abs(y1 - Y0);
+  let sx = X0 < x1 ? 1 : -1;
+  let sy = Y0 < y1 ? 1 : -1;
+  let err = DeltaX - DeltaY;
 
+  while (true) {
+    DibujarPixel(X0, Y0);
+
+    if (X0 === x1 && Y0 === y1) break;
+    var e2 = 2 * err;
+    if (e2 > -DeltaY) {
+      err -= DeltaY;
+      X0 += sx;
+    }
+    if (e2 < DeltaX) {
+      err += DeltaX;
+      Y0 += sy;
+    }
+  }
+}
 function Pendiente(X0: number, Y0: number, X1: number, Y1: number) {
   let m: number = (Y1 - Y0) / (X1 - X0);
   console.log(`Pendiente: (${Y1} - ${Y0}})/(${X1} - ${X0}) =  ${m}`);
@@ -61,173 +209,25 @@ function PendienteInversa(X0: number, Y0: number, X1: number, Y1: number) {
 
 //Funcion para dibujar un solo pixel
 function DibujarPixel(X: number, Y: number) {
-  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  let context: CanvasRenderingContext2D = canvas.getContext(
-    "2d"
-  ) as CanvasRenderingContext2D;
-
   context.fillStyle = "#197BBD";
-  context.fillRect(X + 100, 100 - Y, 3, 3);
+  context.fillRect(X, Y, 3, 3);
   context.stroke();
-}
-
-function MetodoDDA(X0: number, Y0: number, X1: number, Y1: number): number[][] {
-  console.log("Entro Metodo DD");
-  let m: number = Pendiente(X0, Y0, X1, Y1);
-  let mInv: number = PendienteInversa(X0, Y0, X1, Y1);
-  //console.log(`${Y0} - (${m} * ${X0})`)
-
-  let Dx = Math.abs(X0 - X1);
-
-  let Dy = Math.abs(Y0 - Y1);
-
-  let TablaCoordenadas: number[][] = new Array();
-  TablaCoordenadas.push([0, 0]);
-  TablaCoordenadas[0][0] = X0;
-  TablaCoordenadas[0][1] = Y0;
-  function Normal(Valor: number) {
-    return Valor + m;
-  }
-  function ANormal(Valor: number) {
-    return Valor + mInv;
-  }
-
-  if (Dy > Dx) {
-    //Funcion Despejada
-    //Indice Aux mantien el indice anterior durante el ciclo
-    let indiceAux: number = 0;
-    for (let index: number = 1; index < Dy; index++) {
-      let Aux: number = TablaCoordenadas[0][1] + index;
-      let ValorX: number = Math.round(ANormal(TablaCoordenadas[indiceAux][1]));
-      TablaCoordenadas.push([ValorX, Aux]);
-      indiceAux++;
-    }
-  } else {
-    //Se inicializa el array
-    
-    let indiceAux: number = 0;
-    for (let index: number = 1; index < Dx; index++) {
-      let Aux: number = TablaCoordenadas[0][0] + index;
-      // console.log(`${TablaCoordenadas[0][0]} + ${index} = ${Aux}`);
-      //console.log(index);
-      let ValorY: number = Math.round(Normal(TablaCoordenadas[indiceAux][1]));
-      TablaCoordenadas.push([Aux, ValorY]);
-      indiceAux++;
-    }
-  }
-  return TablaCoordenadas;
-}
-function MetodoBresenHan(X0: number, Y0: number, X1: number, Y1: number){
-  // Funcion LineaBresenham( X1, Y1, X2, Y2)
-  //     // 0 - Distancias que se desplazan en cada eje
-  //     dY = (Y2 - Y1)
-  //     dX = (X2 - X1)
-   
-  //     // 1 - Incrementos para las secciones con avance inclinado
-  //     Si (dY >= 0) luego
-  //         IncYi = 1
-  //     Sino
-  //         dY = -dY
-  //         IncYi = -1
-  //     Fin si
-   
-  //     Si (dX >= 0) luego
-  //         IncXi = 1
-  //     Sino
-  //         dX = -dX
-  //         IncXi = -1
-  //     Fin si
-
-  let Dy:number = (Y1-Y0);
-  let Dx:number =(X1-X0);
-  let IncYi:number,IncXi:number;
-  if(Dy>=0){
-    IncYi=1
-  }else{
-    Dy= -Dy;
-    IncYi = -1
-  }
-  if(Dx>=0){
-    IncXi=1;
-  }else{
-    Dx= -Dx;
-    IncXi=-1;
-  }
-  //     // 2 - Incrementos para las secciones con avance recto:
-  //     Si (dX >= dY) luego
-  //         IncYr = 0
-  //         IncXr = IncXi
-  //     Sino
-  //         IncXr = 0
-  //         IncYr = IncYi
-   
-  //         // Cuando dy es mayor que dx, se intercambian, para reutilizar el mismo bucle.
-  //         // ver octantes blancos en la imagen encima del código
-  //         k = dX: dX = dY: dY = k
-  //     Fin si
-  let IncXr:number=0,IncYr:number=0;
-   if(Dx>=Dy){
-     IncYr=0
-     IncXr=IncXi
-   }else{
-     
-   }
-  //     // 3  - Inicializar valores (y de error).
-  //     X = X1: Y = Y1
-  //     avR = (2 * dY)
-  //     av = (avR - dX)
-  //     avI = (av - dX)
-   
-  //     // 4  - Bucle para el trazado de las línea.
-  //     Hacer
-  //         DibujarPixel(X, Y, Color) // Como mínimo se dibujará siempre 1 píxel (punto).
-  //         Mensaje(av + " ") // (debug) para ver los valores de error global que van apareciendo.
-  //         Si (av >= 0) luego
-  //             X = (X + IncXi)     // X aumenta en inclinado.
-  //             Y = (Y + IncYi)     // Y aumenta en inclinado.
-  //             av = (av + avI)     // Avance Inclinado
-  //         Sino
-  //             X = (X + IncXr)     // X aumenta en recto.
-  //             Y = (Y + IncYr)     // Y aumenta en recto.
-  //             av = (av + avR)     // Avance Recto
-  //         Fin si
-  //     Repetir hasta que (X = X2) // NOTA: La condición de 'Repetir Hasta', se debe cambiar si se elige 'Repetir Mientras'
-  //  Fin funcion
-
-
-
 }
 
 function DibujarLineaDDA(X0: number, Y0: number, X1: number, Y1: number) {
   console.log(X0 + " + " + Y0);
   console.log(X1 + " + " + Y1);
 
-  let TablaCoordenadas: number[][] = new Array();
-  TablaCoordenadas = MetodoDDA(X0, Y0, X1, Y1);
-  console.log(TablaCoordenadas);
-  DibujarPixel(X0,Y0);
+  DibujarPixel(X0, Y0);
+  MetodoDDA(X0, Y0, X1, Y1);
   DibujarPixel(X1, Y1);
-  console.log(TablaCoordenadas.length);
-  for (let i = 0; i < TablaCoordenadas.length; i++) {
-    let X: number = TablaCoordenadas[i][0];
-    let Y: number = TablaCoordenadas[i][1];
-    DibujarPixel(X, Y);
-  }
 }
 
 function DibujarLineaDirecta(X0: number, Y0: number, X1: number, Y1: number) {
   console.log(X0 + " + " + Y0);
   console.log(X1 + " + " + Y1);
-
-  let TablaCoordenadas: number[][] = new Array();
-  TablaCoordenadas = MetodoBasico(X0, Y0, X1, Y1);
-  console.log(TablaCoordenadas);
   DibujarPixel(X0, Y0);
+  MetodoDirecto(X0, Y0, X1, Y1);
+
   DibujarPixel(X1, Y1);
-  console.log(TablaCoordenadas.length);
-  for (let i = 0; i < TablaCoordenadas.length; i++) {
-    let X: number = TablaCoordenadas[i][0];
-    let Y: number = TablaCoordenadas[i][1];
-    DibujarPixel(X, Y);
-  }
 }
